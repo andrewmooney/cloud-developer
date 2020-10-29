3d765a150be9f234b3ac2ebe3fc88444dc9fe6e5
Andrew Mooney <amooney@me.com>
2020-10-29 19:11:53 +1000

    Adds types to server.ts

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -31,14 +31,14 @@ import { filterImageFromURL, deleteLocalFiles } from './util/util';
 
   //! END @TODO1
 
-  app.get("/filteredimage", async (req, res) => {
-    const { image_url } = req.query;
+  app.get("/filteredimage", async (req: express.Request, res: express.Response) => {
+    const image_url: string = req.query.image_url;
 
     if (!image_url) {
       return res.status(400).send("A valid image URL is required");
     }
 
-    const filteredImage = await filterImageFromURL(image_url);
+    const filteredImage: string = await filterImageFromURL(image_url);
 
     return res.status(200).sendFile(filteredImage, () => {
       deleteLocalFiles([filteredImage]);
@@ -48,7 +48,7 @@ import { filterImageFromURL, deleteLocalFiles } from './util/util';
   // Root Endpoint
 
   // Displays a simple message to the user
-  app.get("/", async (req, res) => {
+  app.get("/", async (req: express.Request, res: express.Response) => {
     res.send("try GET /filteredimage?image_url={{}}")
   });
 
